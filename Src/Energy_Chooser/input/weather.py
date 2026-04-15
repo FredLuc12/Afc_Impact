8802239269517a53c1ac84b2e25c3f6dd53c41f2
lylyss97x <alyssapaygambar@gmail.com>
2026-04-15 12:09:40 +0200

    APA : correction-weather

--- Src/Energy_Chooser/input/weather.py
+++ Src/Energy_Chooser/input/weather.py
@@ -4,4 +4,4 @@ from constant import url
 def get_weather():
     temp = requests.get(url.API_METEO).text
     temp_formatted = temp.replace("Â°C", "").replace("°C", "").replace("+", "").strip()
-    return temp_formatted
+    return int(temp_formatted)
